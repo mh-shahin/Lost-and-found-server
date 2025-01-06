f6649acac2cc7e6bb97defe3920e125e4c6e93b3
ss0379182@gmail.com <108196088+shahin200131@users.noreply.github.com>
2025-01-06 09:44:51 +0600

    database uri store at .env file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
-const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ouim.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+const DB_URI = process.env.MONGODB_URI;
 
 //establishing mongodb connection
 // mongoose
